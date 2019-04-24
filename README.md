b8fe2ebe27ba0e91033affcf46802baeb2d65a64
micpen <m28126584@gmail.com>
2019-04-25 01:33:39 +0800

    說明欄

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# KerasCustomized
+# KerasCustomized 客製化的keras模型
+
+## 包含功能.
+-包含簡單的卷積神經網路(CNN)跟長短期記憶卷積神經網路模型(LSTM)
+-產生loss圖表
+-產生訓練紀錄檔
+-產生h5訓練後檔案
+
+## 開始:準備訓練與測試資料
+準備對應的numpy型態x_train, y_train訓練資料
+預設使用mnist資料集
+x_train:(60000, 28, 28)#(資料數,資料欄,資料列)
+y_train:(60000,)#(結果數,)
+
+------------------
+
+## 依照資料以及需求調整參數
+
+```json
+
+{
+  "data": {
+    "input_dim": 28,
+    "input_timesteps": 28,
+    "normalise": true,
+    "num_classes": 10
+  },
+  "model": {
+    "loss": "mse",
+    "optimizer": "adam",
+    "plan": "cnn",
+    "save_dir": "saved_models"
+  },
+  "training": {
+    "batch_size": 39,
+    "epochs": 10,
+    "name": "tech_project"
+  }
+}
+
+
+```
+
--- run.py
+++ run.py
@@ -4,13 +4,10 @@ import numpy as np
 import json
 import time
 import keras
-import datetime as dt
-import matplotlib.pyplot as plt
 from keras.datasets import mnist
 from sklearn.model_selection  import train_test_split
 def run_task(configs,x_train, y_train):
 	starttime=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-	print(x_train.shape)
 	model = Model()
 	if(configs['model']["plan"]=="cnn"):
 		model.build_cnn_model(configs)
@@ -36,16 +33,16 @@ def predict(model,x_test=[],y_test=[]):
 	f.close()
 	print(np.mean(arg_y == max_pred))
 def main():
+	#find mnist data set , you can change your dataset
 	(x_train, y_train), (x_test, y_test) = mnist.load_data()
-	configs = json.load(open('config.json', 'r'))
 
+	configs = json.load(open('config.json', 'r'))
+	#cnn need more 1 row
 	if(configs['model']["plan"]=="cnn"):
 		x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2],1)
 		x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2],1)
 	y_train = keras.utils.to_categorical(y_train, configs["data"]["num_classes"])#2=>[0,0,1,0,0,0,0,0]
 	y_test = keras.utils.to_categorical(y_test, configs["data"]["num_classes"])
-	#for i in range(4):
-	#getDL=utils.DataLoader(max_size=configs['model']['layers'][0]["input_timesteps"],tag="classify",classNum=configs['model']['layers'][-1]["neurons"],window=configs['data']['window'])
 	final_model=run_task(configs,x_train, y_train)
 	predict(final_model,x_test, y_test)
 	final_model.find_graph();
