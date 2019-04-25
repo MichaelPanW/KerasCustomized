299040990c7911a7651bddc90db11043187d45be
micpen <m28126584@gmail.com>
2019-04-25 10:28:29 +0800

    校正正確率

--- run.py
+++ run.py
@@ -21,9 +21,18 @@ def run_task(configs,x_train, y_train):
 	)
 	endtime=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 	f = open('log.csv','a')
-	f.write(configs['training']['name']+","+starttime+","+endtime+","+str(x_train.shape)+","+str(configs['training']['epochs'])+","+model.saved_models)
+	f.write(configs['training']['name']+','+starttime+','+endtime+',"'+str(x_train.shape)+'",'+str(configs['training']['epochs'])+','+model.saved_models)
 	f.close()
 	return model
+def loadModel(configs,m5Name):
+	model = Model()
+	if(configs['model']["plan"]=="cnn"):
+		model.build_cnn_model(configs)
+	if(configs['model']["plan"]=="lstm"):
+		model.build_lstm_model(configs)
+	model.load_model(m5Name)
+	return model
+
 def predict(model,x_test=[],y_test=[]):
 	predicted = model.predict_point_by_point(x_test)
 	max_pred=np.argmax(predicted, axis=1)
@@ -32,18 +41,29 @@ def predict(model,x_test=[],y_test=[]):
 	f.write(","+str(np.mean(arg_y == max_pred))+"\n")
 	f.close()
 	print(np.mean(arg_y == max_pred))
+
 def main():
 	#find mnist data set , you can change your dataset
 	(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
 	configs = json.load(open('config.json', 'r'))
+	#dataset pre process 
 	#cnn need more 1 row
 	if(configs['model']["plan"]=="cnn"):
 		x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2],1)
 		x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2],1)
-	y_train = keras.utils.to_categorical(y_train, configs["data"]["num_classes"])#2=>[0,0,1,0,0,0,0,0]
+	#normalization
+	x_train = x_train.astype('float32')
+	x_test = x_test.astype('float32')
+	x_train /= 255
+	x_test /= 255
+	#2=>[0,0,1,0,0,0,0,0]
+	y_train = keras.utils.to_categorical(y_train, configs["data"]["num_classes"])
 	y_test = keras.utils.to_categorical(y_test, configs["data"]["num_classes"])
+	#train your model
 	final_model=run_task(configs,x_train, y_train)
+	
+	#final_model=loadModel(configs,"saved_models/tech_project-cnn-10-e25042019-100839.h5")
+	
 	predict(final_model,x_test, y_test)
 	final_model.find_graph();
 if __name__=='__main__':
